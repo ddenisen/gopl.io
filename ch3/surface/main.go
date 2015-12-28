b139591bf641b10a34ba57121d9875b09af8a94c
Dmitry Denisenkov <dmitry@denisenkov.ca>
2015-12-27 18:26:31 -0800

    Ex 3.2: Support saddle and moguls functions

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -10,7 +10,11 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
+
+// PlotFunc is a mathematical function of type z = f(x,y)
+type PlotFunc func(float64, float64) float64
 
 const (
 	width, height = 600, 320            // canvas size in pixels
@@ -24,15 +28,34 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
+	var function PlotFunc = f
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "sin":
+			function = f
+		case "saddle":
+			function = saddle
+		case "moguls":
+			function = moguls
+		default:
+			fmt.Fprintf(os.Stderr, "Unsupported function: %s\n", os.Args[1])
+			os.Exit(1)
+		}
+	}
+	plot(function)
+}
+
+func plot(function PlotFunc) {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, aOk := corner(i+1, j)
-			bx, by, bOk := corner(i, j)
-			cx, cy, cOk := corner(i, j+1)
-			dx, dy, dOk := corner(i+1, j+1)
+			ax, ay, aOk := corner(i+1, j, function)
+			bx, by, bOk := corner(i, j, function)
+			cx, cy, cOk := corner(i, j+1, function)
+			dx, dy, dOk := corner(i+1, j+1, function)
 
 			// Don't plot invalid coordinates...
 			if !(aOk && bOk && cOk && dOk) {
@@ -46,13 +69,13 @@ func main() {
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64, bool) {
+func corner(i, j int, function PlotFunc) (float64, float64, bool) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
-	z := f(x, y)
+	z := function(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
@@ -72,6 +95,14 @@ func isValid(val float64) bool {
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
+}
+
+func saddle(x, y float64) float64 {
+	return (x*x - y*y) / 512
+}
+
+func moguls(x, y float64) float64 {
+	return (math.Sin(x) + math.Sin(y+3.0*math.Pi/4)) / 16
 }
 
 //!-
